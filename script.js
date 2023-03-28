d68f6afca8e17fa1623a87666539b03d2486db94
Ahmad <ahmad@ahmd-asdy.local>
2023-03-28 04:47:17 +0330

    first version commit

--- script.js
+++ script.js
@@ -1,23 +1,92 @@
+const words = ["programming", "fundamental", "technical", "medication", "approach", "disaster", "global", "intermediate", "billionaire"];
+const container = document.getElementById("container");
+const wrong = document.getElementById("wrong");
+const gameOver = document.getElementById("gameOver");
+const congrat = document.getElementById("congrats");
+const repeat = document.getElementById("repeat");
+const wrongLetters = [];
+const rightLetters = [];
+let wrongIndex = 0;
+const indexes = [];
+
+function chooseWord(wordArr) {
+  const num = wordArr.length;
+  let rand = Math.floor(Math.random() * num);
+  const randomWord = wordArr[rand];
+  console.log(randomWord);
+  return randomWord;
+}
+
+function wordArr(word) {
+  const wordLength = word.length;
+  const wordArray = [];
+  for (let i = 0; i < wordLength; i++) {
+    wordArray.push(word[i]);
+  }
+  return wordArray;
+}
+
+function wordPlaceHolder(wordArray) {
+  for (let i = 0; i < wordArray.length; i++) {
+    const letterSpan = document.createElement("div");
+    letterSpan.className = "letter";
+    letterSpan.id = `_${i}`;
+    container.appendChild(letterSpan);
+  }
+}
+
+function checkLetter(wordArray, letter) {
+  if (wordArray.includes(`${letter}`) && !rightLetters.includes(`${letter}`)) {
+    rightLetters.push(letter);
+  } else if (!wordArray.includes(`${letter}`) && !wrongLetters.includes(`${letter}`)) {
+    wrongLetters.push(letter);
+    wrongIndex++;
+  } else if (rightLetters.includes(`${letter}`) || wrongLetters.includes(`${letter}`)) {
+    repeated();
+  }
+
+  wordArray.forEach((element, i) => {
+    if (element == letter) {
+      document.getElementById(`_${i}`).innerText = letter;
+      wordLength--;
+      if (!indexes.includes(i)) {
+        indexes.push(i);
+      }
+    }
+  });
+  if (indexes.length == wordArray.length) {
+    win();
+  }
+  console.log("indexes: ", indexes);
+  console.log("wrongLetters: ", wrongLetters);
+  console.log("rightLetters: ", rightLetters);
+  drawMan(wrongIndex);
+}
+
+//canvas primary settings
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-let x = innerWidth / 2;
-let y = innerHeight / 2;
+let x = 50;
+let y = 200;
 ctx.strokeStyle = "#FFFFFF";
 ctx.lineWidth = 4;
-ctx.moveTo(x - 50, y);
-ctx.lineTo(x + 50, y);
-ctx.moveTo(x, y);
-ctx.lineTo(x, y - 200);
-ctx.lineTo(x + 100, y - 200);
-ctx.lineTo(x + 100, y - 160);
-ctx.stroke();
+
+function hang() {
+  ctx.moveTo(x - 50, y);
+  ctx.lineTo(x + 50, y);
+  ctx.moveTo(x, y);
+  ctx.lineTo(x, y - 200);
+  ctx.lineTo(x + 100, y - 200);
+  ctx.lineTo(x + 100, y - 160);
+  ctx.stroke();
+}
 
 function head() {
   ctx.beginPath();
   ctx.arc(x + 100, y - 140, 20, 0, 2 * Math.PI);
   ctx.stroke();
 }
-function drawBody() {
+function Body() {
   ctx.moveTo(x + 100, y - 120);
   ctx.lineTo(x + 100, y - 60);
   ctx.stroke();
@@ -42,12 +111,67 @@ function rightHand() {
   ctx.lineTo(x + 130, y - 120);
   ctx.stroke();
 }
-function drawMan() {
-  head();
-  drawBody();
-  leftLeg();
-  rightLeg();
-  leftHand();
-  rightHand();
+function drawMan(i) {
+  switch (i) {
+    case 1:
+      head();
+      break;
+    case 2:
+      Body();
+      break;
+    case 3:
+      leftLeg();
+      break;
+    case 4:
+      rightLeg();
+      break;
+    case 5:
+      leftHand();
+      break;
+    case 6:
+      rightHand();
+      break;
+    case 7:
+      lost();
+  }
+  chooseWord(words);
 }
-drawMan();
+
+const word = chooseWord(words);
+const wordArray = wordArr(word);
+let wordLength = word.length;
+
+hang();
+wordPlaceHolder(wordArray);
+
+function hangMan(rightLetters, wrongLetters) {
+  wrong.innerText = wrongLetters.toString();
+  for (let i = 0; i < rightLetters.length; i++) {}
+}
+
+function repeated() {
+  repeat.classList.add("active");
+  setTimeout(() => {
+    repeat.classList.remove("active");
+  }, 1800);
+}
+
+function lost() {
+  gameOver.classList.add("active");
+}
+
+function win() {
+  congrat.classList.add("active");
+}
+
+hangMan(rightLetters, wrongLetters, words);
+
+window.addEventListener("keypress", (event) => {
+  const letter = event.key;
+  console.log(letter);
+  checkLetter(wordArray, letter);
+  hangMan(rightLetters, wrongLetters, wordArray);
+  if (indexes.length == wordArray.length) {
+    win();
+  }
+});
